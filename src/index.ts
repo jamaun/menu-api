3775a5db3c99fd558d797b4256f8e076bc7e289f
aun Sadiq <aun.sadiq@bentley.com>
2021-04-19 15:03:24 +0500

    swagger implementation

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import helmet from "helmet";
 import { itemsRouter } from "./items/items.router";
 import { errorHandler } from "./middleware/error.middleware";
 import { notFoundHandler } from "./middleware/not-found.middleware";
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
 dotenv.config();
 
 
@@ -31,10 +33,11 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/menu/items", itemsRouter);
-
 app.use(errorHandler);
 app.use(notFoundHandler);
+
 /**
  * Server Activation
  */
